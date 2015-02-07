3a185725149d06b98da37e9eafa0af85e54643a7
Petar Petkov <petar.b.petkov@gmail.com>
2015-02-07 18:27:59 +0200

    Paladin config constats Barbarian config constants Troll config constants Goblin config constants Orc config constants

--- WtfOopaGame/GameEngine/Config.cs
+++ WtfOopaGame/GameEngine/Config.cs
@@ -11,5 +11,40 @@ namespace GameEngine
     /// </summary>
     public static class Config
     {
-    }
+        //Barbarian base constants
+        public static readonly int BarbarianRange = 100;
+        public static readonly int BarbarianAttackPoints = 10;
+        public static readonly int BarbarianDefensePoints = 10;
+        public static readonly int BarbarianHealthPoints = 100;
+        public static readonly int BarbarianAttackSpeed = 50;
+        public static readonly int BarbarianLevel = 1;
+        //Paladin base constants
+        public static readonly int PaladinRange = 100;
+        public static readonly int PaladinAttackPoints = 10;
+        public static readonly int PaladinDefensePoints = 10;
+        public static readonly int PaladinHealthPoints = 100;
+        public static readonly int PaladinAttackSpeed = 50;
+        public static readonly int PaladinLevel = 1;
+        //Goblin base constants
+        public static readonly int GoblinRange = 100;
+        public static readonly int GoblinAttackPoints = 10;
+        public static readonly int GoblinDefensePoints = 10;
+        public static readonly int GoblinHealthPoints = 100;
+        public static readonly int GoblinAttackSpeed = 50;
+        public static readonly int GoblinLevel = 1;
+        //Orc base constants
+        public static readonly int OrcRange = 100;
+        public static readonly int OrcAttackPoints = 10;
+        public static readonly int OrcDefensePoints = 10;
+        public static readonly int OrcHealthPoints = 100;
+        public static readonly int OrcAttackSpeed = 50;
+        public static readonly int OrcLevel = 1;
+        //Troll base constants
+        public static readonly int TrollRange = 100;
+        public static readonly int TrollAttackPoints = 10;
+        public static readonly int TrollDefensePoints = 10;
+        public static readonly int TrollHealthPoints = 100;
+        public static readonly int TrollAttackSpeed = 50;
+        public static readonly int TrollLevel = 1;
+     }
 }
