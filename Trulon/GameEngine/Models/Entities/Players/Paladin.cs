ef04b827bea42eb59bc474f831b49111a7252fe1
kur6uma666 <kur6uma666@gmail.com>
2015-02-12 01:43:24 +0200

    updated Entities.

--- Trulon/GameEngine/Models/Entities/Players/Paladin.cs
+++ Trulon/GameEngine/Models/Entities/Players/Paladin.cs
@@ -1,4 +1,5 @@
-﻿namespace GameEngine.Models.Entities.Players
+﻿using System.Collections.Generic;
+namespace GameEngine.Models.Entities.Players
 {
     using System.Collections.Generic;
 
@@ -16,6 +17,7 @@
         private const int AttackSkill = 0;
         private const int HealthSkill = 0;
         private const int DefenceSkill = 0;
+<<<<<<< HEAD
 
         protected Paladin()
             : base(
@@ -34,6 +36,26 @@
             DefenceSkill)
         {
 
+=======
+
+        protected Paladin()
+            : base(
+            Name,
+            AttackPoints,
+            DefencePoints,
+            SpeedPoints,
+            HealthPoints,
+            Level,
+            new List<Item>(),
+            Experience,
+            Coins,
+            SkillPoints,
+            AttackSkill,
+            HealthSkill,
+            DefenceSkill)
+        {
+
+>>>>>>> e38fed55bf23c1f3d88d539bb114967d3135231b
         }
     }
 }
