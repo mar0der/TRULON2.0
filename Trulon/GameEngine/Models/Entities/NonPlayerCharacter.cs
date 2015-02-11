4f80c3e4d0266df7cfaec612874a252f41618203
MartinKurshumov <kur6uma666@users.noreply.github.com>
2015-02-12 01:45:14 +0200

    Update NonPlayerCharacter.cs

--- Trulon/GameEngine/Models/Entities/NonPlayerCharacter.cs
+++ Trulon/GameEngine/Models/Entities/NonPlayerCharacter.cs
@@ -9,21 +9,13 @@
             int attackPoints, 
             int defencePoints, 
             int speedPoints, 
-            int healthPoints, 
-<<<<<<< HEAD
+            int healthPoints,
             int level,
             List<Item> inventory)
             : base(name, attackPoints, defencePoints, speedPoints, healthPoints, level, inventory)
         {
 
         }
-=======
-            int level, 
-            List<Item> inventory)
-            : base(name, attackPoints, defencePoints, speedPoints, healthPoints, level, inventory)
-        {
->>>>>>> e38fed55bf23c1f3d88d539bb114967d3135231b
-
-        }
+        
     }
 }
