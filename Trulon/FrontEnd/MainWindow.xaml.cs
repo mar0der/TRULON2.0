ac9273819eb2328d174caee30bf8ff168d8a99e2
Petar Petkov <petar.b.petkov@gmail.com>
2015-02-11 18:31:22 -0600

    WPF runs engine.Init() once and calling engine.Update() 25 - 30 times a second

--- Trulon/FrontEnd/MainWindow.xaml.cs
+++ Trulon/FrontEnd/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameEngine;
 
 namespace FrontEnd
 {
@@ -23,6 +24,9 @@ namespace FrontEnd
         public MainWindow()
         {
             InitializeComponent();
+            Engine engine = new Engine();
+            engine.Init();
+            CompositionTarget.Rendering += (sender, args) => engine.Update();
         }
     }
 }
---
+++ Trulon/GameEngine/Engine.cs
@@ -0,0 +1,21 @@
+﻿namespace GameEngine
+{
+    public class Engine
+    {
+        private int i = 0;
+        public Engine()
+        {
+            
+        }
+
+        public void Init()
+        {
+
+        }
+
+        public void Update()
+        {
+            System.Console.WriteLine(this.i++);
+        }
+    }
+}
