c37ae5af7ef99c1001b7219235c56d74759bdfc9
Petar Petkov <petar.b.petkov@gmail.com>
2015-02-14 11:56:11 +0200

    Item generator static class added

---
+++ Trulon2.0/Trulon2.0/CoreLogics/ItemGenerator.cs
@@ -0,0 +1,51 @@
+﻿namespace Trulon.CoreLogics
+{
+    using GameEngine.Models;
+    using GameEngine.Models.Items.Equipments;
+    using GameEngine.Models.Items.Potions;
+    using System;
+    static class  ItemGenerator
+    {
+        private static Random rand = new Random();
+        public static Item GetEquipmentItem()
+        {
+            switch (rand.Next(0,3))
+            {
+                case 0:
+                    return new Boots();
+                    break;
+                case 1:
+                    return new Helmet();
+                    break;
+                case 2:
+                    return new Vest();
+                    break;
+                default:
+                    throw new Exception("Something went wrong in Item Generator");
+                    break;
+            }
+        }
+
+        public static Item GetPotionItem()
+        {
+                        switch (rand.Next(0,3))
+            {
+                case 0:
+                    return new DamagePotion();
+                    break;
+                case 1:
+                    return new DefencePotion();
+                    break;
+                case 2:
+                    return new HealthPotion();
+                    break;
+                case 3:
+                    return new SpeedPotion();
+                    break;
+                default:
+                    throw new Exception("Something went wrong in Item Generator");
+                    break;
+            }
+        }
+    }
+}
