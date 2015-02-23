b247418f047bb13e68906c2d6f387bc37d567ed8
Petar Petkov <petar.b.petkov@gmail.com>
2015-02-23 23:10:55 +0200

    MapManager added

---
+++ Trulon2.0/Trulon2.0/CoreLogics/MapManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Trulon.CoreLogics
+{
+    using System;
+
+    public class MapManager
+    {
+        public event EventHandler MapUp;
+
+        protected void OnMapUp()
+        {
+            if (MapUp != null)
+            {
+                MapUp(this, new EventArgs());
+            }
+        }
+
+        public void FireMapUp()
+        {
+            OnMapUp();
+        }
+    }
+
+}
